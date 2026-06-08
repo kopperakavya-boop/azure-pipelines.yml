db50854389cf1ffa310b4aca69baac3c25f00a8d
kopperakavya-boop <koppera.kavya@gmail.com>
2026-06-08 15:08:41 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~>4.0"
     }
   }
 }
@@ -13,59 +11,34 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "rg-devops-vm"
+  location = "Central India"
 }
 
-# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "win10-vnet"
-  address_space       = ["10.0.0.0/16"]
+  name                = "vnet-devops"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
-# Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "win10-subnet"
+  name                 = "subnet-devops"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP
 resource "azurerm_public_ip" "pip" {
-  name                = "win10-pip"
+  name                = "pip-devops"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Standard"
 }
 
-# Network Security Group
-resource "azurerm_network_security_group" "nsg" {
-  name                = "win10-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "Allow-RDP"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "win10-nic"
+  name                = "nic-devops"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -77,39 +50,28 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Associate NSG with NIC
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Windows 10 Virtual Machine
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = var.vm_name
+  name                = "win10-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
 
-  size           = "Standard_B2s_v2"
-  admin_username = var.admin_username
-  admin_password = var.admin_password
+  admin_username = "azureadmin"
+  admin_password = "P@ssw0rd1234!"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
 
   os_disk {
-    name                 = "${var.vm_name}-osdisk"
     caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
+    offer     = "windows-10"
     sku       = "win10-22h2-pro"
     version   = "latest"
   }
-
-  enable_automatic_updates = true
-  patch_mode               = "AutomaticByOS"
 }
